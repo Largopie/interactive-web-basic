d03d949d2e23b205862e36ab5d2c80b79716ff2a
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-25 10:10:06 +0900

    x좌표 좌우로 입체적으로 움직이도록 구현

--- sirup/js/sirup.js
+++ sirup/js/sirup.js
@@ -1,7 +1,17 @@
 let x = 0, y = 0;
 
-const contentAll = document.querySelectorAll(".contWrap div");
-console.log(contentAll);
+
+const contentAll = document.querySelectorAll(".contWrap img");
+
+// console.log(contentAll);
+
+
+const shadow = contentAll[0];
+const date = contentAll[1];
+const human = contentAll[2];
+const textImg = contentAll[3];
+
+// console.log(human);
 
 window.addEventListener("mousemove", (event) => {
   x = event.pageX - window.innerWidth / 2;
@@ -10,10 +20,18 @@ window.addEventListener("mousemove", (event) => {
   // console.log(y);
 
   mouseMoveFunc();
+  
 });
 
 const mouseMoveFunc = () => {
   // contentAll.forEach((item) => {
   //   item.style.left = x + "px";
   // });
+
+  shadow.style.transform = `translateX(${x / 35}px)`;
+  date.style.transform = `translateX(${x / 15}px)`;
+  human.style.transform = `translateX(${-x / 30}px)`;
+  textImg.style.transform = `translateX(${-x / 15}px)`;
 };
+
+
