3d24907182aee60a1109de69ee689603f9fb5545
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-30 02:55:30 +0900

    블로그 페이지 페럴렉스 구현 완료

--- 06blog/js/index.js
+++ 06blog/js/index.js
@@ -3,27 +3,31 @@ const progressBar = document.querySelector(".bar");
 const coverTitle = document.querySelector(".coverTitle");
 const coverWrap = document.querySelector(".coverWrap");
 const dimd = document.querySelector(".dimd");
+const contentWrap = document.querySelector(".contentWrap");
 
 let scrollNum = 0;
 let documentHeight = 0;
 let per = 0;
+const coverHeight = window.innerHeight;
+
+coverWrap.style.height = coverHeight + "px";
+contentWrap.style.marginTop = coverHeight + "px";
 
 window.addEventListener("scroll", () => {
   scrollNum = window.scrollY;
-  documentHeight = document.body.scrollHeight - window.innerHeight + 400;
+  documentHeight = document.body.scrollHeight - window.innerHeight + coverHeight;
 
   per = percent(scrollNum, documentHeight);
   progressBar.style.width = per + "%";
 
-  if (scrollNum >= 400) {
+  if (scrollNum >= coverHeight) {
     header.classList.add("fixed");
   } else {
-    coverTitle.style.top = - scrollNum / 5 + "px";
+    coverTitle.style.top = scrollNum / 10 + "px";
     header.classList.remove("fixed");
   }
-
-  coverWrap.style.backgroundPosition = `center ${ -scrollNum / 10 }px`
-  dimd.style.backgroundColor = `rgba(0, 0, 0, ${ 0.4 + scrollNum / 700 })`;
+  coverWrap.style.backgroundPosition = `center ${ -scrollNum / 3 }px`
+  dimd.style.backgroundColor = `rgba(0, 0, 0, ${ 0.4 + scrollNum / 1000 })`;
 })
 
 const percent = (num, totalNum) => {
