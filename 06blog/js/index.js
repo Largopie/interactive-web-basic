fd0e8c0511b45b0406681a9c56a5565cb9c100c4
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-30 01:49:05 +0900

    사진이 끝나면 프로그래스 바 출현 및 헤더 고정

--- 06blog/js/index.js
+++ 06blog/js/index.js
@@ -1,13 +1,24 @@
+const header = document.querySelector("header");
 const progressBar = document.querySelector(".bar");
+
+let scrollNum = 0;
+let documentHeight = 0;
+let per = 0;
 
 window.addEventListener("scroll", () => {
   scrollNum = window.scrollY;
   documentHeight = document.body.scrollHeight - window.innerHeight + 400;
-  
+
   per = percent(scrollNum, documentHeight);
   progressBar.style.width = per + "%";
 
-  
+  if (scrollNum >= 400) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+
+
 })
 
 const percent = (num, totalNum) => {
