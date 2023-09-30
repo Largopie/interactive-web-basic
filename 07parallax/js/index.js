3b768c699ad25ea29a8021bdbc3c3545f41e5fe6
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-01 01:26:31 +0900

    마우스 움직임에 따라 페럴렉스 효과 구현

--- 07parallax/js/index.js
+++ 07parallax/js/index.js
@@ -5,7 +5,7 @@ let windowHeight = 0;
 const imageAll = document.querySelectorAll(".imageWrap .parallaxImage");
 const totalNum = imageAll.length;
 const subPageImage = document.querySelector(".subPage .parallaxImage");
-
+let x = 0;
 
 window.addEventListener("scroll", () => {
   scrollNum = window.scrollY;
@@ -13,5 +13,14 @@ window.addEventListener("scroll", () => {
   imageAll.forEach((item, idx) => {
     item.style.transform = `translateY(${ -scrollNum / (2 * (totalNum -idx))}px)`;
   })
-}) 
+})
+
+window.addEventListener("mousemove", (e) => {
+  x = e.pageX - window.innerWidth / 2;
+  imageAll[4].style.transform = `scale(1.05) translateX(${-x / 50}px)`;
+  imageAll[5].style.transform = `scale(1.05) translateX(${-x / 100}px)`;
+  subPageImage.style.transform = `scale(1.1) translateX(${-x / 20}px)`
+  
+})
+
 
